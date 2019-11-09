a290ffb0d28846dd7c9cfa093e49514be3f84c6a
MinKyeongTae <kyeongtea92@gmail.com>
2019-11-09 20:50:38 +0900

    Add Memo/MemoryStorage Class

---
+++ RxMemo/RxMemo/Model/Memo.swift
@@ -0,0 +1,28 @@
+//
+//  Memo.swift
+//  RxMemo
+//
+//  Created by MinKyeongTae on 09/11/2019.
+//  Copyright © 2019 MinKyeongTae. All rights reserved.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String // 메모 구분에 사용하는 프로퍼티
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        // insertDate의 2001/1/1 UTC 시간 기준, TimeInterval 값을 identity에 넣음
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(original: Memo, updatedContent: String) {
+        self = original
+        self.content = updatedContent
+    }
+    
+}
---
+++ RxMemo/RxMemo/Service/MemoStorageType.swift
@@ -0,0 +1,26 @@
+//
+//  MemoStorageType.swift
+//  RxMemo
+//
+//  Created by MinKyeongTae on 09/11/2019.
+//  Copyright © 2019 MinKyeongTae. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+// 프로토콜 메서드의 정의 방식)
+// 구독자가 옵저바블을 원하는대로 처리할 수 있도록 반환형을 옵저바블로 선언한다. 반대로 작업결과가 필요 없는 경우를 위해 @discardableResult를 선언했다.
+protocol MemoStorageType {
+    @discardableResult
+    func createMemo(content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func memoList() -> Observable<[Memo]>
+    
+    @discardableResult
+    func update(memo: Memo, content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func delete(memo: Memo) -> Observable<Memo>
+}
