ae33c80253cf0818ef0d6e87147da51b28ecda9d
MinKyeongTae <kyeongtea92@gmail.com>
2020-05-15 00:26:44 +0900

    Review Lecture #3 for RxMemo

--- RxMemo/RxMemo/ViewController/ViewModelBindableType.swift
+++ RxMemo/RxMemo/ViewController/ViewModelBindableType.swift
@@ -15,9 +15,9 @@ protocol ViewModelBinableType {
     func bindViewModel()
 }
 
-// 뷰컨트롤러에 추가 된 뷰모델 속성의 실제 뷰모델을 저장, 바인드 뷰모델 메소드를 자동으로 호출하는 메서드를 구현한다.
+// 뷰컨트롤러에 추가 된 뷰모델 속성의 실제 ViewModel을 저장하고, bind() 뷰모델 메서드를 자동으로 호출하는 메서드를 구현한다.
 extension ViewModelBinableType where Self: UIViewController {
-    //  바인드 뷰모델 메소드를 자동으로 호출하는 메서드를 구현한다.
+    // 바인드 뷰모델 메소드를 자동으로 호출하는 메서드를 구현한다.
     mutating func bind(viewModel: Self.ViewModelType) {
         self.viewModel = viewModel
         loadViewIfNeeded()
