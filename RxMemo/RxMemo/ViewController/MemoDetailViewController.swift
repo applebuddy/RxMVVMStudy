5c8787d84a433904dcf4e07a8b780a589487908f
MinKyeongTae <kyeongtea92@gmail.com>
2020-05-17 23:55:50 +0900

    Study Lecture #7-1 for RxMemo; step 14) 메모보기 화면구현

--- RxMemo/RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/RxMemo/ViewController/MemoDetailViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class MemoDetailViewController: UIViewController, ViewModelBinableType {
+    // MARK: - Property
+
+    // 07-01) commit(05:36)
+    @IBOutlet var listTableView: UITableView!
+    @IBOutlet var editButton: UIBarButtonItem!
+    @IBOutlet var deleteButton: UIToolbar!
+    @IBOutlet var shareButton: UIBarButtonItem!
+
     var viewModel: MemoDetailViewModel!
 
     override func viewDidLoad() {
