10ed1cdde0791da391eeff394e209c34d270275e
MinKyeongTae <kyeongtea92@gmail.com>
2020-05-24 16:52:00 +0900

    Study Lecture #7-2 for RxMemo; step 15) MemoDetailViewModel 구현

--- RxMemo/RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/RxMemo/ViewController/MemoDetailViewController.swift
@@ -23,5 +23,27 @@ class MemoDetailViewController: UIViewController, ViewModelBinableType {
         super.viewDidLoad()
     }
 
-    func bindViewModel() {}
+    // 07-09) 먼저 네비게이션 타이틀을 바인딩 하겠습니다.
+    func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
+        // 07-10) 이어서 테이블 뷰를 바인딩 합니다.
+        viewModel.contents
+            .bind(to: listTableView.rx.items) { tableView, row, value in
+                switch row {
+                case 0:
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "contentCell")!
+                    cell.textLabel?.text = value // 10:41
+                    return cell
+                case 1:
+                    guard let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell") else { return UITableViewCell() }
+                    cell.textLabel?.text = value
+                    return cell // 11:07
+                default:
+                    fatalError()
+                }
+            }
+            .disposed(by: rx.disposeBag)
+    }
 }
--- RxMemo/RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -6,7 +6,38 @@
 //  Copyright © 2019 MinKyeongTae. All rights reserved.
 //
 
+import Action
 import Foundation
+import RxCocoa
+import RxSwift
 
 /// 메모보기 화면에서 사용하는 뷰 모델
-class MemoDetailViewModel {}
+class MemoDetailViewModel: CommonViewModel {
+    // 07-06) 뷰 모델에 사용할 몇가지 속성을 추가하겠습니다.
+    let memo: Memo
+
+    // MARK: formatter는 날자를 문자열로 바꿀때 사용합니다.
+
+    private var formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "Ko_kr")
+        f.dateStyle = .medium
+        f.timeStyle = .medium
+        return f
+    }()
+
+    // 07-07) 메모 내용은 테이블 뷰에 표시됩니댜.
+    // 왜 기본 옵저버블이 아닌 BehaviorSubject일까요?? 메모보기 이후 편집된 메모를 저장하고 보기화면으로 왔을때의 새로운 문자열 배열을 방출할 수 있도록 할 수 있는 BehaviorSubject를 사용할 수 있습니다. 이어서 생성자를 생성 및 모든 속성을 초기화하겠습니다.
+    var contents: BehaviorSubject<[String]>
+
+    init(memo: Memo, title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+        self.memo = memo
+
+        contents = BehaviorSubject<[String]>(value: [
+            memo.content,
+            formatter.string(from: memo.insertDate),
+        ])
+
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
+}
